2331083ab4c331f6aa44ecc16049933e93728e18
Selin Gulmez <115948361+Selinngulmez@users.noreply.github.com>
2023-12-28 20:52:05 +0300

    Patient package is added.

---
+++ CS320-Project-main/src/src/Patient/Patient.java
@@ -0,0 +1,32 @@
+package Patient;
+
+public class Patient {
+    private int patient_id;
+    private String first_name;
+    private String last_name;
+    private String phone_num;
+
+    public Patient(int patient_id, String first_name, String last_name, String phone_num) {
+        this.patient_id = patient_id;
+        this.first_name = first_name;
+        this.last_name = last_name;
+        this.phone_num = phone_num;
+    }
+
+    public int getPatientId() {
+        return patient_id;
+    }
+
+    public String getFirstName() {
+        return first_name;
+    }
+
+    public String getLastName() {
+        return last_name;
+    }
+
+    public String getPhoneNum() {
+        return phone_num;
+    }
+}
+
