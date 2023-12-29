a29ceff47a83a238f1999db15512b5bb0d5c8091
selensonmezz <selen.sonmez@ozu.edu.tr>
2023-12-30 00:05:50 +0300

    Clinic class constructor added, getters updated

--- CS320-Project-main/src/src/Clinic/Clinic.java
+++ CS320-Project-main/src/src/Clinic/Clinic.java
@@ -2,24 +2,26 @@ package Clinic;
 
 public class Clinic {
     private String clinicName;
-    private String ClinicAddress;
+    private String clinicAddress;
     private int clinicId;
     private String contactDetails;
 
 
+    public Clinic() {}
 
 
-    public int getClinicId() {}
+    public int getClinicId() {return clinicId;}
 
     public String getClinicName() {
-
+return clinicName;
     }
 
     public String getLocation() {
+        return clinicAddress;
     }
 
     public String getContactDetails() {
-
+return contactDetails;
     }
 
 }
