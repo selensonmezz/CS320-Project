c2962143ebad0f7e529d6169dbe940310d325897
caglacengiz <cagla.cengiz@ozu.edu.tr>
2024-01-01 16:33:25 +0300

    constants class is added
    this class defining some parameters but if we don't need it later, it might be deleted

---
+++ CS320-Project-main/src/src/Main/Constants.java
@@ -0,0 +1,16 @@
+package Main;
+
+public class Constants {
+
+    // Database connection parameters
+    public static final String DB_URL = "";
+    public static final String DB_USER = "";
+    public static final String DB_PASSWORD = "";
+
+    // Date format
+    public static final String DATE_FORMAT = "yyyy-MM-dd";
+
+    // Other constants
+    public static final int MAX_LOGIN_ATTEMPTS = 3;
+
+}
