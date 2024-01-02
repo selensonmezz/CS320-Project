4d2573330fd53b0907bc972d5068c95dfe86fec5
caglacengiz <cagla.cengiz@ozu.edu.tr>
2024-01-03 00:24:01 +0300

    delete unused method from appointmentdataaccess

--- CS320-Project-main/src/src/Appointment/AppointmentDataAccess.java
+++ CS320-Project-main/src/src/Appointment/AppointmentDataAccess.java
@@ -54,28 +54,6 @@ public class AppointmentDataAccess {
         return appointments;
     }
 
-    public List<Appointment> listAppointmentByAppointmentId(int appointmentId) {
-        List<Appointment> appointments = new ArrayList<>();
-        try {
-            String sql = "SELECT * FROM appointment WHERE appointment_id = ?";
-            PreparedStatement preparedStatement = databaseConnection.prepareStatement(sql);
-            preparedStatement.setInt(1, appointmentId);
-            ResultSet resultSet = preparedStatement.executeQuery();
-
-            while (resultSet.next()) {
-                int appointment_id = resultSet.getInt("appointment_id");
-                Date date = resultSet.getDate("date");
-                String time = resultSet.getString("time");
-                String prescription = resultSet.getString("prescription");
-                String patientName = resultSet.getString("patient_name");
-                appointments.add(new Appointment(appointment_id, date, time, patientName, prescription));
-            }
-        } catch (SQLException e) {
-            e.printStackTrace();
-        }
-        return appointments;
-    }
-
     public void updateAppointment(Appointment appointment) {
         try {
             String sql = "UPDATE appointment SET date = ?, time = ?, patient_name = ?, notes = ? WHERE appointment_id = ?";
